ab4ec8a80bcea2cf77b789de0ce346c2219c9c1d
Ashratuz Zavin Asha <ashzavin@gmail.com>
2024-07-20 02:06:19 -0600

    Files

---
+++ Assets/button_grab_control.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button_grab_control : MonoBehaviour
+{
+    public GameObject anVideo1;
+    //public GameObject prayer_time_greet;
+    //public GameObject prayer_muslim_man;
+    //public GameObject prayer_muslim_man1;
+
+    //public GameObject way_char2;
+    //public GameObject prayer_recite;
+
+    int flag_interfaith = 0;
+    void Update()
+    {
+        if (GetComponent<OVRGrabbable>().m_grabbedBy != null)
+        {
+            anVideo1.SetActive(true);
+        }
+    }
+}
