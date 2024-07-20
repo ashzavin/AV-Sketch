23b9d4fd73d72fe711bbdc96c7d4811a2d39bebb
Ashratuz Zavin Asha <ashzavin@gmail.com>
2024-07-20 08:52:28 -0600

    Files

---
+++ Assets/welcome_message_control.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class welcome_message_control : MonoBehaviour
+{
+    public GameObject wel_chi;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("Welcome_Play", 5);
+    }
+
+    public void Welcome_Play()
+    {
+        wel_chi.SetActive(true);
+    }
+}
